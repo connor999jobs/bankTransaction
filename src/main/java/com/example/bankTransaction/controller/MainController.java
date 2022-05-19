748622c2ea56064f4f5016ddedbf7304bf71b68d
connor999jobs <coonor999jobs@gmail.com>
2022-05-19 06:05:40 +0300

    Initial commit

---
+++ src/main/java/com/example/bankTransaction/controller/MainController.java
@@ -0,0 +1,57 @@
+package com.example.bankTransaction.controller;
+
+import com.example.bankTransaction.dao.BankAccountDAO;
+import com.example.bankTransaction.exception.BankTransactionException;
+import com.example.bankTransaction.form.SendMoneyForm;
+import com.example.bankTransaction.model.BankAccountInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private BankAccountDAO bankAccountDAO;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showBankAccounts(Model model) {
+        List<BankAccountInfo> list = bankAccountDAO.listBankAccountInfo();
+
+        model.addAttribute("accountInfos", list);
+
+        return "accountsPage";
+    }
+
+    @RequestMapping(value = "/sendMoney", method = RequestMethod.GET)
+    public String viewSendMoneyPage(Model model) {
+
+        SendMoneyForm form = new SendMoneyForm(1L, 2L, 700d);
+
+        model.addAttribute("sendMoneyForm", form);
+
+        return "sendMoneyPage";
+    }
+
+
+    @RequestMapping(value = "/sendMoney", method = RequestMethod.POST)
+    public String processSendMoney(Model model, SendMoneyForm sendMoneyForm) {
+
+        System.out.println("Send Money: " + sendMoneyForm.getAmount());
+
+        try {
+            bankAccountDAO.sendMoney(sendMoneyForm.getFromAccountId(), //
+                    sendMoneyForm.getToAccountId(), //
+                    sendMoneyForm.getAmount());
+        } catch (BankTransactionException e) {
+            model.addAttribute("errorMessage", "Error: " + e.getMessage());
+            return "/sendMoneyPage";
+        }
+        return "redirect:/";
+    }
+
+}
